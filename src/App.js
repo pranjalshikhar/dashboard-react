d022d5c8fa74822dd0b475319437b62a33713917
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2022-12-03 13:07:59 +0530

    Router Setup 📍

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Dashboard } from "@mui/icons-material";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import Bar from "./scenes/bar/Bar";
+import Calendar from "./scenes/calendar/Calendar";
+import Contacts from "./scenes/contacts/Contacts";
+import FAQ from "./scenes/faq/FAQ";
+import Form from "./scenes/form/Form";
+import Geography from "./scenes/geography/Geography";
 import TopBar from "./scenes/global/TopBar";
+import Invoices from "./scenes/invoices/Invoices";
+import Line from "./scenes/line/Line";
+import Pie from "./scenes/pie/Pie";
+import SideBar from "./scenes/global/SideBar";
+import Team from "./scenes/team/Team";
 import { ColorModeContext, useColorMode } from "./themes";
 
 function App() {
@@ -9,8 +22,22 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <SideBar />
           <main className="content">
             <TopBar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/geography" element={<Geography />} />
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
