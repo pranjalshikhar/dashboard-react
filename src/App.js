c8d34d30a913e3c71af991eea4d4ee37654c456d
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2022-12-05 17:09:05 +0530

    homepage

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
           <main className="content">
             <TopBar />
             <Routes>
-              <Route path="/" element={<Dashboard />} />
+              <Route path="/dashboard-react" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
